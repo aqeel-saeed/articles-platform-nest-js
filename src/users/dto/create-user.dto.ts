1cf04ab5114defa058de5485e7d7deaf35357535
aqeel_saeed <aqeelraiee210@gmail.com>
2023-12-07 16:42:54 +0300

    Fill User Entity And DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,40 @@
-export class CreateUserDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Match } from '../../decorators/match.decorators';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    // validate that the password has uppercase letters, lowercase letters, numbers and special chars
+    message: 'password too weak',
+  })
+  @ApiProperty()
+  password: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @IsNotEmpty()
+  @ApiProperty()
+  @Match('password')
+  passwordConfirm: string;
+}
