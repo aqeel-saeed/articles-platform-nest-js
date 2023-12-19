3d66cef1b86a83b2464ea3593ea84761106ca868
aqeel_saeed <aqeelraiee210@gmail.com>
2023-12-19 13:53:01 +0300

    Exclude Password From Response

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,12 @@
 import { User } from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
 
 export class UserEntity implements User {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+
   @ApiProperty()
   id: number;
 
@@ -17,5 +22,6 @@ export class UserEntity implements User {
   @ApiProperty()
   email: string;
 
+  @Exclude()
   password: string;
 }
